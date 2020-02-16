102f50e6fa64a312a2867ca4c834c43148196d93
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-02-16 10:21:46 +0000

    Empty of project

--- HAPI_Start/HAPI_Start/myEnum.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//Used by multiple classes to define entity type. Not ideal but was ease of access.
-enum eEntityType
-{
-	eTypeBackground, eTypeEnemy, eTypePlayer,eTypeNeutral, eTypeBullet, eTypeExplosion
-};
